da966b82ed6422a60b91f73caa3953305c3c4ada
Philip Kiely <Philip@Philips-MacBook-Air.local>
2018-01-27 07:50:10 -0600

    comp upper and lower instances

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var forbidden = ["is", "are", "was", "were"];
 
+function lower(string) {
+    return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
 document.getElementById("grade").addEventListener("click", function() {
   var out = [];
   var essay = document.getElementById("page").value;
-  var sentences = essay.split(/[.!?]+/); //add regex .!?
+  var sentences = essay.split(/[.!?]+/);
   for (i = 0; i < sentences.length; i++) {
     sentences[i] = sentences[i].replace(/[|&;$%@"<>():'=`–\\_#^*+,]/g, "");
-    var words = sentences[i].split(" "); //add regex all delims
+    var words = sentences[i].split(" ");
     var words_so_far = []
     for (j = 0; j < words.length; j++) {
       if (forbidden.indexOf(words[j]) > -1) {
@@ -24,6 +28,7 @@ document.getElementById("grade").addEventListener("click", function() {
         out.push(" ");
       }//else
       words_so_far.push(words[j]);
+      words_so_far.push(lower(words[j]));
       if (j == words.length - 1) {
         out.pop()
       }//if end, removes extra space
@@ -34,9 +39,7 @@ document.getElementById("grade").addEventListener("click", function() {
     }//if end, removes extra period
     out.push("</span> "); //just in case
   }//for sentence in essay
-  console.log(out)
   var out_string = out.join("")
-  console.log(out_string)
   document.getElementById("output").innerHTML = out_string;
   document.getElementById("page").classList.add("hidden");
   document.getElementById("output").classList.remove("hidden");
