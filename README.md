3ac77bd7646617cc033529a72715cc60258e02d1
Philip Kiely <Philip@Philips-MacBook-Air.local>
2018-01-26 22:36:40 -0600

    works but removes all punctuation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # AutoGarrison
 
-Copy and paste your assignment text, then press "grade" to flag the forbidden words (is, are, was, were) and any repeated words in a sentence.
+Copy and paste your assignment text, then press "grade" to flag the forbidden words (is, are, was, were) and any repeated words in a sentence. Run on Internet Explorer at your own risk.
--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 var forbidden = ["is", "are", "was", "were"];
 
-
 document.getElementById("grade").addEventListener("click", function() {
-  var essay = document.getElementById("page").innerHTML;
+  var out = [];
+  var essay = document.getElementById("page").value;
   var sentences = essay.split("."); //add regex .!?
-  for(i = 0; i < sentences.length; i++) {
+  for (i = 0; i < sentences.length; i++) {
     var words = sentences[i].split(" "); //add regex all delims
-    for(j = 0; j < words.length) {
-      if (word[i] in forbidden) {
-        //highlight the word
+    var words_so_far = []
+    for (j = 0; j < words.length; j++) {
+      if (forbidden.indexOf(words[j]) > -1) {
+        out.push("<span style=\"color: red;\">");
+        out.push(words[j]);
+        out.push("</span> ");
       }//if forbidden
+      else if (words_so_far.indexOf(words[j]) > -1) {
+        out.push("<span style=\"color: red;\">");
+        out.push(words[j]);
+        out.push("</span> ");
+      }//if repeated
+      else {
+        out.push(words[j]);
+        out.push(" ");
+      }//else
+      words_so_far.push(words[j]);
+      if (j == words.length - 1) {
+        out.pop()
+      }//if end, removes extra space
     }// for word in sentence
+    out.push(". ")
+    if (i == sentences.length - 1) {
+      out.pop()
+    }//if end, removes extra period
   }//for sentence in essay
+  console.log(out)
+  var out_string = out.join("")
+  console.log(out_string)
+  document.getElementById("output").innerHTML = out_string;
+  document.getElementById("page").classList.add("hidden");
+  document.getElementById("output").classList.remove("hidden");
+  document.getElementById("grade").classList.add("hidden");
 });
